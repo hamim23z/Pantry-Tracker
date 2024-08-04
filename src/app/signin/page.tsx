7395974934054325914676f9e8192619d8ec51b2
Hamim <hamimc232@gmail.com>
2024-08-04 17:27:13 -0400

    Added New Image

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -33,11 +33,11 @@ export default function Signin() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+            className="mx-auto h-[200px] w-[200px]"
+            src="https://imgur.com/bVEGgXv.png"
             alt="Your Company"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white font-kanit">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
             Sign In to Your Account
           </h2>
         </div>
